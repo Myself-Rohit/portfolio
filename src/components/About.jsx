28ad63a4c638a327c1ac72fa407e24ca233c7929
rohit negi <rohitnegi263139@gmail.com>
2024-10-18 21:40:29 +0530

    add blogPulse in projects and updated about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,71 @@
-import React from 'react'
-import photoAbout from '../images/photoAbout.jpg'
-import Skills from './Skills'
-import resume from '../assets/Resume.pdf'
+import React from "react";
+import photoAbout from "../images/photoAbout.jpg";
+import Skills from "./Skills";
+import resume from "../assets/Resume.pdf";
 
 function About() {
-  return (
-    <div id="about" className="flex flex-col items-center pt-10 sm:pt-5 bg-gradient-to-r from-black to-slate-900" >
-      <div className="sm:mx-10">
-        <h1 className="text-white text-4xl font-bold ">
-          About<span className="text-cyan-600">Me</span>
-        </h1>
-        <div className="sm:flex ">
-          <div className="flex flex-col justify-evenly bg-gradient-to-t from-slate-100 to-slate-500  p-4 sm:mr-4 rounded-2xl items-center ">
-            <p className="max-w-screen-md shrink-0">
-              Hello there! I'm an aspiring web developer with a strong desire to break into the tech industry. I have a foundational understanding of HTML, CSS, Tailwind CSS, JavaScript, React, Redux, Git and Github. I'm eager to expand my skill set and delve deeper into the world of web development. I'm committed to continuous learning and growth, and I'm excited about the prospect of contributing to innovative projects and collaborating with like-minded professionals. 
-             
-            </p>
-            <div className="flex items-start justify-around w-full text-gray-900">
-              <div className="flex text-sm flex-col">
-                <p className="font-bold px-2 rounded-sm py-1">Name <span className="text-xs font-medium ml-1">ROHIT NEGI</span></p>
-                <p className="font-bold px-2 rounded-sm w-full py-1">Age <span className="text-xs font-medium ml-1">21</span></p>
-                <p className="font-bold px-2 rounded-sm w-full py-1">Resume <a className="text-blue-500 font-normal ml-1" href={resume} target="_blank">Veiw</a></p>
-              </div>
-              <a href={resume} download="Rohit's Resume">
-                <div className=" duration-300 bg-gradient-to-tl from-cyan-400 to-cyan-800 font-medium text-gray-300 hover:bg-gradient-to-br border border-cyan-700
-             px-7 py-1 rounded-md inline-block">
-                  Download Resume
-                </div></a>
-            </div>
-          </div>
-          <div className="bg-cyan-600 h-80 relative lg:block hidden shrink-0">
-            <img className="rotate-1 h-80 object-cover " src={photoAbout} />
-          </div>
-        </div>
-      </div>
-      <Skills/>
-    </div>
-  )
+	return (
+		<div
+			id="about"
+			className="flex flex-col items-center pt-10 sm:pt-5 bg-gradient-to-r from-black to-slate-900"
+		>
+			<div className="sm:mx-10">
+				<h1 className="text-white text-4xl font-bold ">
+					About<span className="text-cyan-600">Me</span>
+				</h1>
+				<div className="sm:flex ">
+					<div className="flex flex-col justify-evenly bg-gradient-to-t from-slate-100 to-slate-500  p-4 sm:mr-4 rounded-2xl items-center ">
+						<p className="max-w-screen-md shrink-0">
+							{/* Hello there! I'm an aspiring web developer with a strong desire to break into the tech industry. I have a foundational understanding of HTML, CSS, Tailwind CSS, JavaScript, React, Redux, Git and Github. I'm eager to expand my skill set and delve deeper into the world of web development. I'm committed to continuous learning and growth, and I'm excited about the prospect of contributing to innovative projects and collaborating with like-minded professionals.  */}
+							Hello there! I am a aspiring web developer with a Bachelor's
+							degree in Computer Applications, set to graduate in 2025. I have a
+							solid foundation in HTML, CSS, Tailwind CSS, JavaScript, React,
+							Node.js, Express.js and MongoDB, along with version control tools
+							like Git and GitHub. I am eager to apply my skills in real-world
+							scenarios I am eager to continuously expand my knowledge and dive
+							deeper into modern web technologies. With a commitment to learning
+							and innovation, I am excited about the opportunity to contribute
+							to dynamic teams, collaborate with like-minded professionals, and
+							bring fresh ideas to real-world projects.
+						</p>
+						<div className="flex items-start justify-around w-full text-gray-900">
+							<div className="flex text-sm flex-col">
+								<p className="font-bold px-2 rounded-sm py-1">
+									Name{" "}
+									<span className="text-xs font-medium ml-1">ROHIT NEGI</span>
+								</p>
+								<p className="font-bold px-2 rounded-sm w-full py-1">
+									Age <span className="text-xs font-medium ml-1">22</span>
+								</p>
+								<p className="font-bold px-2 rounded-sm w-full py-1">
+									Resume{" "}
+									<a
+										className="text-blue-500 font-normal ml-1"
+										href={resume}
+										target="_blank"
+									>
+										Veiw
+									</a>
+								</p>
+							</div>
+							<a href={resume} download="Rohit's Resume">
+								<div
+									className=" duration-300 bg-gradient-to-tl from-cyan-400 to-cyan-800 font-medium text-gray-300 hover:bg-gradient-to-br border border-cyan-700
+             px-7 py-1 rounded-md inline-block"
+								>
+									Download Resume
+								</div>
+							</a>
+						</div>
+					</div>
+					<div className="bg-cyan-600 h-80 relative lg:block hidden shrink-0">
+						<img className="rotate-1 h-80 object-cover " src={photoAbout} />
+					</div>
+				</div>
+			</div>
+			<Skills />
+		</div>
+	);
 }
 
-export default About
+export default About;
--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,19 +1,30 @@
-import starbooks from '../images/starbooks.png'
-import weatherapp from '../images/weatherapp.jpg'
+import starbooks from "../images/starbooks.png";
+import weatherapp from "../images/weatherapp.jpg";
+import blogpulse from "../images/blogpulse.png";
 const projects = [
-    {
-        title: "Star",
-        subtitle:"Books",
-        image: starbooks,
-        url:"https://creative-faun-9f1d5a.netlify.app/",
-        descripion:"StarBooks is a online bookstore website featuring cutting-edge technologies such as ReactJS with Vite, Tailwind CSS, React Router, and Redux Toolkit. Key features include dynamic routing, asynchronous API calls for book data, responsive design, efficient search functionality, detailed book information, cart management, seamless checkout via the Google Play Store, and indicators for online-offline mode switching. "
-    },
-    {
-        title:"weather",
-        subtitle:"App",
-        image: weatherapp,
-        url:"https://classy-crisp-440ebb.netlify.app/",
-        descripion:"This Weather report apps include a search feature that allows users to input the name or location of a place they're interested in. Once a location is entered, the app retrieves and displays current weather conditions"
-    },
-]
-export default projects
+	{
+		title: "Blog",
+		subtitle: "Pulse",
+		image: blogpulse,
+		url: "https://blogpulse-iyt9.onrender.com/",
+		descripion:
+			"BlogPulse is a dynamic blog platform built using the MERN stack (MongoDB, Express.js, React, and Node.js) and Flowbite for UI components. It lets users like, comment, search for posts, and manage their profiles. The site also includes a dark/light mode toggle for a better user experience. An admin panel allows admins to easily create, edit, or delete posts. The platform is designed to be responsive, meaning it works well on all devices.",
+	},
+	{
+		title: "Star",
+		subtitle: "Books",
+		image: starbooks,
+		url: "https://creative-faun-9f1d5a.netlify.app/",
+		descripion:
+			"StarBooks is a online bookstore website featuring cutting-edge technologies such as ReactJS with Vite, Tailwind CSS, React Router, and Redux Toolkit. Key features include dynamic routing, asynchronous API calls for book data, responsive design, efficient search functionality, detailed book information, cart management, seamless checkout via the Google Play Store, and indicators for online-offline mode switching. ",
+	},
+	{
+		title: "weather",
+		subtitle: "App",
+		image: weatherapp,
+		url: "https://classy-crisp-440ebb.netlify.app/",
+		descripion:
+			"This Weather report apps include a search feature that allows users to input the name or location of a place they're interested in. Once a location is entered, the app retrieves and displays current weather conditions",
+	},
+];
+export default projects;
