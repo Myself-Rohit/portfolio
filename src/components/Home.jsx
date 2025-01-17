7d986b7e14b66889991528c063cd9980e1154a3a
rohit negi <rohitnegi263139@gmail.com>
2025-01-17 13:31:22 +0530

    change skills UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 import { Link } from "react-scroll";
-import photoHome from "../images/photoHome.jpg"
+import photoHome from "../images/photoHome.jpg";
 
 function Home() {
-    return (
-        <>
-            <div id="home" className='flex flex-col sm:flex-row justify-center min-h-screen items-center bg-no-repeat bg-center bg-cover bg-[url("https://media.istockphoto.com/photos/product-showcase-with-spotlight-black-studio-room-background-use-as-picture-id1304681590?b=1&k=20&m=1304681590&s=170667a&w=0&h=UlTsdL4KIRPcxbhgOrsIoE0BNNE6KpgxVhRD3gz_fV0=")]' >
-                <div className="shrink-0">
-                    <img src={photoHome} className="w-72 h-72 rounded-full object-fit object-left border-2 border-cyan-600" />
-                </div>
-                
-                <div className="sm:ml-4">
-                    <h1 className="text-white text-5xl font-bold">
-                        Hi
-                        <span className="text-cyan-600">!</span>
-                        <div>
-                            <span >I<span className="text-cyan-600">'</span>m</span>
-                            <span className="ml-2 text-cyan-500 ">RoHit</span>
-                        </div>
-                    </h1>
+	return (
+		<>
+			<div
+				id="home"
+				className='flex flex-col sm:flex-row justify-center min-h-screen items-center bg-no-repeat bg-center bg-cover bg-[url("https://media.istockphoto.com/photos/product-showcase-with-spotlight-black-studio-room-background-use-as-picture-id1304681590?b=1&k=20&m=1304681590&s=170667a&w=0&h=UlTsdL4KIRPcxbhgOrsIoE0BNNE6KpgxVhRD3gz_fV0=")]'
+			>
+				<div className="shrink-0">
+					<img
+						src={photoHome}
+						className="w-72 h-72 rounded-full object-fit object-left border-2 border-cyan-600"
+					/>
+				</div>
 
-                    <p className="text-transparent bg-clip-text bg-gradient-to-t from-gray-500 to-white max-w-sm text-sm py-4 ">
-                        A passionate front-end web developer.
-                    </p>
-                    
-                    <Link smooth duration={800} to="contact">
-                        <div className="duration-300 bg-gradient-to-br from-cyan-400 to-cyan-800 font-medium text-gray-300 hover:bg-gradient-to-tl hover:scale-105 border border-cyan-700 cursor-pointer
-             px-7 py-1 rounded-md inline-block">
-                            Contact
-                        </div>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+				<div className="sm:ml-4">
+					<h1 className="text-white text-5xl font-bold">
+						Hi
+						<span className="text-cyan-600">!</span>
+						<div>
+							<span>
+								I<span className="text-cyan-600">'</span>m
+							</span>
+							<span className="ml-2 text-cyan-500 ">RoHit</span>
+						</div>
+					</h1>
+
+					<p className="text-transparent bg-clip-text bg-gradient-to-t from-gray-500 to-white max-w-sm text-sm py-4 ">
+						A passionate full stack web developer.
+					</p>
+
+					<Link smooth duration={800} to="contact">
+						<div
+							className="duration-300 bg-gradient-to-br from-cyan-400 to-cyan-800 font-medium text-gray-300 hover:bg-gradient-to-tl hover:scale-105 border border-cyan-700 cursor-pointer
+             px-7 py-1 rounded-md inline-block"
+						>
+							Contact
+						</div>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 }
 export default Home;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -44,21 +44,38 @@ function Skills() {
 			image:
 				"https://cdn1.iconfinder.com/data/icons/picons-social/57/github_rounded-64.png",
 		},
+		{
+			id: 9,
+			title: "NodeJs",
+			image: "https://img.icons8.com/?size=48&id=54087&format=png",
+		},
+		{
+			id: 10,
+			title: "ExpressJs",
+			image: "https://img.icons8.com/?size=48&id=SDVmtZ6VBGXt&format=png",
+		},
+		{
+			id: 11,
+			title: "MongoDB",
+			image: "https://img.icons8.com/?size=48&id=bosfpvRzNOG8&format=png",
+		},
 	];
 	return (
 		<div className="p-10 pb-0 flex flex-col items-center">
 			<h1 className=" font-bold text-4xl text-gray-400 text-center p-4 rounded-xl shadow-md shadow-cyan-600 border border-cyan-400">
 				My Skills
 			</h1>
-			<div className="flex flex-wrap items-center justify-center gap-5 mt-7 py-5">
+			<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5 mt-7 py-5">
 				{mySkills.map((skill) => {
 					return (
-						<div
-							key={skill.id}
-							title={skill.title}
-							className="w-20 h-20 sm:w-28 sm:h-28 border-b-2 hover:border-black hover:scale-95 border-cyan-600 p-3 rounded-full"
-						>
-							<img src={skill.image} className="w-full h-full" />
+						<div className="bg-white flex items-center gap-2 px-2 py-1 rounded-md">
+							<div
+								key={skill.id}
+								className="w-10 h-10 sm:w-20 sm:h-20 border-b-2 hover:border-black hover:scale-95 border-cyan-600 p-2 sm:p-3 rounded-full"
+							>
+								<img src={skill.image} className="w-full h-full" />
+							</div>
+							<p className="font-semibold">{skill.title}</p>
 						</div>
 					);
 				})}
